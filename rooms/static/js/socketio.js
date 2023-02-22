21806faac3dcf651ba23ba2210ec24a172dc6866
Jdubbs702 <702leopold@gmail.com>
2023-02-22 11:01:10 +0200

    set hrefs to elastic beanstalk address

--- rooms/static/js/socketio.js
+++ rooms/static/js/socketio.js
@@ -2,7 +2,8 @@
 const langSelect = document.querySelector('.lang-select');
 langSelect.style.display = 'none';
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io("http://picturethis-env-1.eba-zpfpzxzv.ap-northeast-1.elasticbeanstalk.com")
 
 // user joining socketio room from rooms.html or create_room.html
 socket.on('connect', async() => {
@@ -38,7 +39,7 @@ socket.on('leave_room_announcement', (data) => {
     clearAll();
     announcementElement.style.marginTop = '45px';
     announceWithLoader(`${data.username} ${t('socketio.leftRm')}`);
-    window.location.href = `http://127.0.0.1:5000/rooms/view-room/${data.room_name}/`;
+    window.location.href = `http://picturethis-env-1.eba-zpfpzxzv.ap-northeast-1.elasticbeanstalk.com/rooms/view-room/${data.room_name}/`;
 });
 
 // component initialized after join_room announcment
