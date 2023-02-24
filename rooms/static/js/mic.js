38d0551d8161f7ffc9f3855d06bdfc0f75fcd500
Jdubbs702 <702leopold@gmail.com>
2023-02-24 11:45:44 +0200

    file path

--- rooms/static/js/mic.js
+++ rooms/static/js/mic.js
@@ -28,7 +28,7 @@ let audio;
 let clockOn;
 
 // override user engagement for audio on mobile
-audio = new Audio('../nothing.wav');
+audio = new Audio('http://touchbasicapp.com/nothing.wav');
 document.addEventListener('touchstart', () => {
     audio.play();
 });
