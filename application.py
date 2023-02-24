6afab69271b30e3f2817879e25da16ae30533a41
Jdubbs702 <702leopold@gmail.com>
2023-02-24 07:41:44 +0200

    deleted socketio config

--- application.py
+++ application.py
@@ -125,4 +125,4 @@ def load_user(username):
 
 if __name__ == "__main__":
     # additional socketio.run param: debug=True
-    socketio.run(application, host='0.0.0.0', allow_unsafe_werkzeug=True)
+    socketio.run(application, host='0.0.0.0')
