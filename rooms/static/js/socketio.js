d1315371e930e2d3ddfa03aea458b452e82bdbd3
Jdubbs702 <702leopold@gmail.com>
2023-02-26 12:08:42 +0200

    test

--- rooms/static/js/socketio.js
+++ rooms/static/js/socketio.js
@@ -47,7 +47,7 @@ socket.on('receive_audio', (data) => {
     console.log('srcElement.src:',srcElement.src);
     // console.log('srcElement.srcObject:',srcElement.srcObject);
     console.log(audioTag);
-    audioTag.load();
+    srcElement.load();
     audioTag.play();
 });
 
